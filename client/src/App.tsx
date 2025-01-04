0a0cc555d946cd587dcc0346ea419d83ab64204b
Ardit Bytyci <ab45425@ubt-uni.net>
2025-01-04 13:36:57 +0100

    #19 commit - added AdminDashboard with dummy data for testing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,10 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+} from "react-router-dom";
 import { useAuth } from "./Authentication/AuthContext"; // Import your AuthContext
 import BookView from "./Components/BookView";
 import AuthorView from "./Components/AuthorView";
@@ -10,18 +16,25 @@ import LoginPage from "./Components/LoginPage";
 import RegisterPage from "./Components/RegisterPage";
 import HomeView from "./Components/Home";
 import ProtectedRoute from "../src/Authentication/ProtectedRoute";
+import AdminDashboard from "./Components/AdminComponents/AdminDashboard";
 
 const Navbar: React.FC = () => {
-  const { isLoggedIn, logout } = useAuth();
+  const { isLoggedIn, logout, role } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/Home", { replace: true });
+  };
 
   return (
     <div className="navbar bg-base-100">
       <div className="flex-2">
-        <Link to="/BookView" className="btn btn-ghost text-xl">
-          Books
-        </Link>
         <Link to="/Home" className="btn btn-ghost text-xl">
           Home
+        </Link>
+        <Link to="/BookView" className="btn btn-ghost text-xl">
+          Books
         </Link>
         <Link to="/AuthorView" className="btn btn-ghost text-xl">
           Authors
@@ -29,19 +42,27 @@ const Navbar: React.FC = () => {
         <Link to="/GenreView" className="btn btn-ghost text-xl">
           Genre
         </Link>
-        <Link to="/OrderView" className="btn btn-ghost text-xl">
-          Order
-        </Link>
-        <Link to="/CustomerView" className="btn btn-ghost text-xl">
-          Customer
-        </Link>
-        <Link to="/OrderDetailView" className="btn btn-ghost text-xl">
-          OrderDetail
-        </Link>
+
+        {role === "Admin" && (
+          <>
+            <Link to="/OrderView" className="btn btn-ghost text-xl">
+              Order
+            </Link>
+            <Link to="/CustomerView" className="btn btn-ghost text-xl">
+              Customer
+            </Link>
+            <Link to="/OrderDetailView" className="btn btn-ghost text-xl">
+              OrderDetail
+            </Link>
+            <Link to="/Admin" className="btn btn-ghost text-xl">
+              Admin Dashboard
+            </Link>
+          </>
+        )}
       </div>
       <div>
         {isLoggedIn ? (
-          <button onClick={logout} className="btn btn-warning">
+          <button onClick={handleLogout} className="btn btn-warning">
             Logout
           </button>
         ) : (
@@ -80,6 +101,14 @@ const App: React.FC = () => {
             element={
               <ProtectedRoute requiredRoles={["Admin"]}>
                 <OrderView />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/Admin"
+            element={
+              <ProtectedRoute requiredRoles={["Admin"]}>
+                <AdminDashboard />
               </ProtectedRoute>
             }
           />
